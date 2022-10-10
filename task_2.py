1d4cd38501c3fdca52493daaa7942ee0623bc051
Elena <malkovva@gmail.com>
2022-10-10 11:14:39 +0300

    Adjusted range for task 2

--- task_2.py
+++ task_2.py
@@ -3,9 +3,12 @@ import math
 n = int(input("Введите целое положительное число n: "))
 prime_factors = []
 
-for i in range(2, math.ceil(n**0.5)):
+for i in range(2, math.ceil(n**0.5)+1):
     while n % i == 0:
         prime_factors.append(i)
         n = n/i
 
-print(prime_factors)
+if len(prime_factors) < 1:
+    print("Это простое число")
+else:
+    print(f"Список простых множителей числа n: {prime_factors}")
